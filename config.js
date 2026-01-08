64ce1eb5809b1c3ff28b23ffa32e330bdf3b7219
brandorb73-ship-it <brandorb73@gmail.com>
2026-01-08 21:13:59 +1100

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+  MAPBOX_TOKEN: "YOUR_MAPBOX_TOKEN",
+
+  SHEETS: {
+    DEST_TRADE: { id: "SHEET_ID_1", name: "Destination Countries - Trade Data" },
+    ORIGIN_TRADE: { id: "SHEET_ID_2", name: "Origin Countries - Trade Data" },
+    ENFORCEMENT: { id: "SHEET_ID_3", name: "Origin & Destination - Enforcement News" },
+    ILLICIT: { id: "SHEET_ID_4", name: "Illicit Route Insights" }
+  }
+};
